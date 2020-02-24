1e21e7666723e6acb94bf71d1c56c375113a6ed8
syahirah <syahirah.suradi@gmail.com>
2020-02-24 12:34:19 +0800

    created three components -  about, home, contact. testing react router

---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Contact = () => {
+    return (
+        <div class="container">
+            <h4 class="center">Contact</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </div>
+    )
+}
+
+export default Contact
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div class="container">
+            <h4 class="center">Home</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </div>
+    )
+}
+
+export default Home
